a8fc5a1cdf690c58113cb7447977cabaf8701ce3
Montenegro Cruz <montenegrocruz.uk@icloud.com>
2024-11-13 16:21:16 +0000

    Lista la vista de la aplicacion

---
+++ src/main/java/org/montengro/view/ConsolaView.java
@@ -0,0 +1,60 @@
+package org.montengro.view;
+
+import java.util.Scanner;
+
+/**
+ * La clase ConsolaView proporciona una interfaz de consola para interactuar con el usuario.
+ * Ofrece métodos para mostrar menús, leer entradas de texto y enteros, y mostrar mensajes.
+ */
+public class ConsolaView {
+
+  // Scanner para leer la entrada del usuario desde la consola
+  private Scanner scanner = new Scanner(System.in);
+
+  /**
+   * Muestra el menú principal y lee la opción seleccionada por el usuario.
+   *
+   * @return la opción seleccionada por el usuario
+   */
+  public int mostrarMenu() {
+    System.out.println("\nMenú:");
+    System.out.println("1. Insertar concesionario");
+    System.out.println("2. Insertar coche");
+    System.out.println("3. Listar concesionarios y coches");
+    System.out.println("4. Exportar a JSON y XML");
+    System.out.println("5. Guardar datos y salir");
+    System.out.print("Elija una opción: ");
+    return scanner.nextInt();
+  }
+
+  /**
+   * Lee una entrada de texto del usuario después de mostrar un mensaje.
+   *
+   * @param mensaje el mensaje para mostrar al usuario
+   * @return la entrada de texto del usuario
+   */
+  public String leerTexto(String mensaje) {
+    System.out.println(mensaje);
+    return scanner.nextLine();
+  }
+
+  /**
+   * Lee una entrada entera del usuario después de mostrar un mensaje.
+   *
+   * @param mensaje el mensaje para mostrar al usuario
+   * @return la entrada entera del usuario
+   */
+  public int leerEntero(String mensaje) {
+    System.out.println(mensaje);
+    return scanner.nextInt();
+  }
+
+  /**
+   * Muestra un mensaje en la consola.
+   *
+   * @param mensaje el mensaje para mostrar al usuario
+   */
+  public void mostrarMensaje(String mensaje) {
+    System.out.println(mensaje);
+  }
+}
